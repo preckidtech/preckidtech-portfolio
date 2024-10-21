3bf6addecdc79280832f5a9a2eaca8787bd316a8
Oyebode Precious <preckidtech@gmail.com>
2024-10-21 02:12:46 +0100

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./component/Header";
+import Container from "./component/Container";
+import Skill from "./component/Skill";
+import Box from "./component/Box";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <Container />
+      <Box />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
