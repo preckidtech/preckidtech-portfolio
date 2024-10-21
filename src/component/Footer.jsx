37f4846cb946023d1ecfceae91bdee3673c03345
Oyebode Precious <preckidtech@gmail.com>
2024-10-21 02:20:22 +0100

    added footer component

---
+++ src/component/Footer.jsx
@@ -0,0 +1,96 @@
+import logo from "../assets/logo/preckid logo.png";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+const Footer = () => {
+  const socialHandle = [
+    {
+      handle: "instagram",
+      icon: <FaInstagram />,
+      link: "https://api.whatsapp.com/message/K2JH7N3JZJ6BO1",
+    },
+    {
+      handle: "linkedin",
+      icon: <FaLinkedinIn />,
+      link: "https://api.whatsapp.com/message/K2JH7N3JZJ6BO1",
+    },
+    {
+      handle: "facebook",
+      icon: <FaFacebook />,
+      link: "https://api.whatsapp.com/message/K2JH7N3JZJ6BO1",
+    },
+    {
+      handle: "whatsapp",
+      icon: <FaWhatsapp />,
+      link: "https://api.whatsapp.com/message/K2JH7N3JZJ6BO1",
+    },
+  ];
+  return (
+    <>
+      <section className="bg-[rgb(29,29,28)] py-12 text-white">
+        <section className="grid grid-cols-2 items-center justify-center gap-y-4 w-5/6 m-auto max-sm:grid-cols-1 ">
+          <section className="grid grid-cols-2">
+            <div>
+              <img className="h-8 max-md:h-6 max-sm:h-6" src={logo} alt="" />
+            </div>
+            <div>
+              <p>Web Developer</p>
+              <p>Frontend Developer</p>
+              <p>Email Designer</p>
+            </div>
+          </section>
+          {/* second section */}
+          <section className="grid grid-cols-2">
+            <div>
+              <ul>
+                <li>
+                  <a href="#service-card">Home</a>
+                </li>
+                <li>
+                  <a href="#project-section">My projects.</a>
+                </li>
+                <li>
+                  <a href="#about-section">About me</a>
+                </li>
+                <li>
+                  <a href="#contact-section"></a>Contact me
+                </li>
+                {/* <li>
+                  <a href=""></a>Blog
+                </li> */}
+              </ul>
+            </div>
+            <div>
+              <p>Social media handles</p>
+
+              <div className="flex gap-4 text-[rgb(255,130,255)]">
+                {socialHandle.map(function (data) {
+                  return (
+                    <a href={data.link} target="blank">
+                      <span className="text-xl">{data.icon}</span>
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+        </section>
+
+        {/* Developer acknoledgement */}
+      </section>
+      <section className="bg-[rgb(29,29,28)] text-white py-6 border-t-[1px] border-gray">
+        <p className="text-center max-sm:px-2">
+          2024 Copyright @{" "}
+          <span className="text-[rgb(255,130,255)] font-extrabold">
+            Preckid Tech
+          </span>
+          . Dedicated to{" "}
+          <span className="text-[rgb(255,130,255)] font-extrabold">GOD</span>
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Footer;
